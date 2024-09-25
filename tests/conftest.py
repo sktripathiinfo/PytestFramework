4e840654bb0f73b81b79d57559b4cf1c5679ea6a
sktripathiinfo <sktripathiinfo@gmail.com>
2024-09-25 19:41:06 +0530

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,37 +1,55 @@
-import time
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
+import time
+driver = None
 
-# Adding command line hook
 def pytest_addoption(parser):
     parser.addoption(
-        "--browser", action="store", default="chrome"
+        "--browser_name", action="store", default="chrome"
     )
+
 
 @pytest.fixture(scope="class")
 def setup(request):
-    browser = request.config.getoption("browser")
-    if browser == "chrome":
-        driver = webdriver.Chrome()
-        driver.maximize_window()
-
-    elif browser == "firefox":
-        driver = webdriver.Firefox()
-        driver.maximize_window()
-
-    elif browser == "IE":
-        driver = webdriver.Ie()
-        driver.maximize_window()
-
+    global driver
+    browser_name=request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(executable_path="C:\\geckodriver.exe")
+    elif browser_name == "IE":
+        print("IE driver")
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
+
     request.cls.driver = driver
-
-    # The driver belongs to local driver - driver.get, driver.maximize
-    # and cls.driver belongs to the test_end2end class driver
-
     yield
     driver.close()
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+        driver.get_screenshot_as_file(name)
+
